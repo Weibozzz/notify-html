aaafc82194414b201b31432650f49824de79d1e0
liuweibo <liuweibo@haier.com>
2023-03-14 17:34:55 +0800

    feat: 增加页面激活调用

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 'use strict';
 
+function http(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        resolve(xhr.responseText);
+      }
+    };
+    xhr.send(null);
+  });
+}
 function notify ({
                    url,
                    timer = 30 * 1000,
                    callback = () => {}
                  }) {
+  let timerId;
   if (!url) {
     console.error('notify-html err', 'url 不存在');
     return;
   }
-  let timerId = setInterval(() => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    xhr.onreadystatechange = function () {
-      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        callback(xhr.responseText);
-      }
-    };
-    xhr.send(null);
-  }, timer);
+  const requestFn = () => {
+    http(url).then(data => {
+      callback(data);
+    });
+  };
+  const intervalFn = () => {
+    timerId = setInterval(() => {
+      requestFn();
+    }, timer);
+  };
+  intervalFn();
+  const eventFn = () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+      requestFn();
+    }
+    intervalFn();
+  };
+  window.addEventListener('focus', eventFn);
+  window.addEventListener('visibilitychange', () => {
+    !document.hidden && eventFn();
+  });
   return timerId;
 }
 module.exports = notify;
--- src/main.js
+++ src/main.js
@@ -1,22 +1,48 @@
+function http(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        resolve(xhr.responseText)
+      }
+    }
+    xhr.send(null);
+  });
+}
 function notify ({
                    url,
                    timer = 30 * 1000,
                    callback = () => {}
                  }) {
+  let timerId
   if (!url) {
     console.error('notify-html err', 'url 不存在');
     return;
   }
-  let timerId = setInterval(() => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    xhr.onreadystatechange = function () {
-      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        callback(xhr.responseText)
-      }
+  const requestFn = () => {
+    http(url).then(data => {
+      callback(data)
+    })
+  }
+  const intervalFn = () => {
+    timerId = setInterval(() => {
+      requestFn()
+    }, timer)
+  }
+  intervalFn()
+  const eventFn = () => {
+    if (timerId) {
+      clearInterval(timerId)
+      timerId = null
+      requestFn()
     }
-    xhr.send(null);
-  }, timer)
+    intervalFn()
+  }
+  window.addEventListener('focus', eventFn)
+  window.addEventListener('visibilitychange', () => {
+    !document.hidden && eventFn()
+  })
   return timerId;
 }
 module.exports = notify
